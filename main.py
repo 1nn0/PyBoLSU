f55590fc04957610cab8e2a38b6a0b3278d0b582
1nn0 <rus.y2k@gmail.com>
2015-09-20 19:32:37 +0300

    Some editing and formatting

--- main.py
+++ main.py
@@ -2,35 +2,37 @@ from filecmp import cmp, clear_cache
 from configparser import ConfigParser
 import shutil
 import os
-
 import requests
 
 
 # Check if config is present
-if os.path.isfile("config.ini"):
-    config = ConfigParser()
-    config.read("config.ini")
-    bol_path = config.get('paths', 'bol', raw=True)
-    scripts_path = bol_path + 'Scripts\\'
-    common_path = scripts_path + 'Common\\'
-else:
-    print("Config not found!")
-    conf = ConfigParser()
-    conf['paths'] = {'bol' : os.getcwd()}
-    conf['scripts'] = {'Jinx' : 'https://raw.githubusercontent.com/RalphLeague/BoL/master/Jinx.lua'}
-    conf['common'] = {'SxOrbWalk.lua' : 'https://raw.githubusercontent.com/Superx321/BoL/master/common/SxOrbWalk.lua'}
-    with open("config.ini", 'w') as config_file:
-        conf.write(config_file)
-    print("Config file created. Check config.ini and edit it if necessary.")
-    input('Press enter to exit')
-    exit()
-
-# Define User-Agent header, for compatibility with some servers
-user_agent = {'User-agent': 'Mozilla/5.0'}
+def check_config():
+    if os.path.isfile("config.ini"):
+        config = ConfigParser()
+        config.read("config.ini")
+        bol_path = config.get('paths', 'bol', raw=True)
+        scripts_path = bol_path + 'Scripts\\'
+        common_path = scripts_path + 'Common\\'
+    else:
+        print("Config not found!")
+        conf = ConfigParser()
+        conf['paths'] = {'bol': os.getcwd()}
+        conf['scripts'] = {'Jinx': 'https://raw.githubusercontent.com/RalphLeague/BoL/master/Jinx.lua'}
+        conf['common'] = {
+            'SxOrbWalk.lua': 'https://raw.githubusercontent.com/Superx321/BoL/master/common/SxOrbWalk.lua'}
+        with open("config.ini", 'w') as config_file:
+            conf.write(config_file)
+        print("Config file created. Check config.ini and edit it if necessary.")
+        input('Press enter to exit')
+        exit()
+    return config, scripts_path, common_path
 
 
 # Download and compare remote files against local files
 def update(section, path):
+    # Define User-Agent header, for compatibility with some servers
+    user_agent = {'User-agent': 'Mozilla/5.0'}
+
     for option in config.options(section):
         # Get file from URL to temp file.
         req = requests.get(config.get(section, option, raw=True), headers=user_agent)
@@ -53,7 +55,8 @@ def update(section, path):
             shutil.copyfile("tmp.lua", path + option)
     return "Ok"
 
-
+#Call all the functions!
+config, scripts_path, common_path = check_config()
 update('scripts', scripts_path)
 update('common', common_path)
 
