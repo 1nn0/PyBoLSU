8fe632a46fba02d7f38017133822f831c82f6e87
1nn0 <rus.y2k@gmail.com>
2015-09-20 17:38:16 +0300

    PEP8 kinda format

--- main.py
+++ main.py
@@ -1,38 +1,42 @@
 from filecmp import cmp, clear_cache
-import requests
 from configparser import ConfigParser
 import shutil
 import os
 
-#Check if config is present
+import requests
+
+
+# Check if config is present
 if os.path.isfile("config.ini"):
     config = ConfigParser()
     config.read("config.ini")
-    scripts_path = config.get('paths', 'bol', raw=True)
+    bol_path = config.get('paths', 'bol', raw=True)
+    scripts_path = bol_path + 'Scripts\\'
     common_path = scripts_path + 'Common\\'
 else:
     print("Config not found!")
     exit()
 
-#Define User-Agent header, for compatibility with some servers
-user_agent = {'User-agent' : 'Mozilla/5.0'}
+# Define User-Agent header, for compatibility with some servers
+user_agent = {'User-agent': 'Mozilla/5.0'}
 
-#Download and compare remote files against local files
+
+# Download and compare remote files against local files
 def update(section, path):
     for option in config.options(section):
-        #Get file from URL to temp file.
-        req = requests.get(config.get(section, option, raw=True), headers = user_agent)
+        # Get file from URL to temp file.
+        req = requests.get(config.get(section, option, raw=True), headers=user_agent)
         with open("tmp.lua", "wb") as tmp:
             tmp.write(req.content)
         tmp.close()
 
-    #Add .lua extension if not present.
+        # Add .lua extension if not present.
         if ".lua" not in option:
             option = option + ".lua"
 
-    # Check and compare files in a simple if-else
+        # Check and compare files in a simple if-else
         if not os.path.isfile(path + option):
-            print("No file! Copying " + option )
+            print("No file! Copying " + option)
             shutil.copyfile("tmp.lua", path + option)
         elif cmp("tmp.lua", path + option, shallow=False):
             print("Files identical! " + option)
@@ -41,11 +45,12 @@ def update(section, path):
             shutil.copyfile("tmp.lua", path + option)
     return "Ok"
 
+
 update('scripts', scripts_path)
 update('common', common_path)
 
 
-#Cleanup and exit
+# Cleanup and exit
 clear_cache()
 os.remove("tmp.lua")
 print("All done!")
